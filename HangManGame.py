60ebe7f135705089f254e636f28e26724af89ce9
rase6 <94676275+rase6@users.noreply.github.com>
2023-06-16 20:45:14 +0100

    Update HangManGame.py

--- HangManGame.py
+++ HangManGame.py
@@ -6,7 +6,7 @@ class HangmanGame:
     def __init__(self):
         # Initialize the game with necessary variables
         self.words_to_guess = ["chocolates", "books", "photography", "cinema", "city", "peace", "magazines", "toys", "songs", "parents", "legos"]
-        self.limit = 5 # Number of guesses allowed
+        self.limit = 8 # Number of guesses allowed
         self.count = 0  # Current number of incorrect guesses
         self.already_guessed = []  # List to store already guessed letters
         self.word = ""    # Word to be guessed
@@ -135,7 +135,7 @@ class HangmanGame:
             "  |     O \n"
             "  |    /| \n"
             "  |       \n"
-            "__|__\n"
+            "__|__\n",
             
              "   _____ \n"
             "  |     | \n"
@@ -144,7 +144,7 @@ class HangmanGame:
             "  |     O \n"
             "  |    /|\ \n"
             "  |       \n"
-            "__|__\n"
+            "__|__\n",
             
              "   _____ \n"
             "  |     | \n"
@@ -153,7 +153,7 @@ class HangmanGame:
             "  |     O \n"
             "  |    /|\ \n"
             "  |    /  \n"
-            "__|__\n"
+            "__|__\n",
             
              "   _____ \n"
             "  |     | \n"
